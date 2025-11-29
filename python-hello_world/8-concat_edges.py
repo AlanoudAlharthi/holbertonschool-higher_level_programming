b01593661ba26657c55611498372d529cb8b2de7
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-29 16:11:46 +0000

    task7

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = "programming with Python"
-# print the concatenated sentence
-print(str1 + " " + str2)
+# print the full sentence using the existing variables
+print(word1 + " " + word2 + " " + word3)
