45a590f9a458900cefd940fab78a5ef40f07cb65
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-29 17:44:06 +0000

    add task 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import marshal
+import hidden_4
 
-with open("/tmp/hidden_4.pyc", "rb") as f:
-    f.read(16)  # skip header
-    code = marshal.load(f)
-
-for name in sorted(code.co_names):
-    if not name.startswith("__"):
-        print(name)
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
