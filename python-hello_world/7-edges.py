0fb16e7f76bfd5f63177c9a84e06b75219381f04
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-29 16:02:39 +0000

    task5

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,9 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-# print the first 3 letters
 print("First 3 letters:", word_first_3)
-# print the last 2 letters
 print("Last 2 letters:", word_last_2)
-# print the middle letters
 print("Middle word:", middle_word)
